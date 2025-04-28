3f3fdbd8421bb2b498b9b60e6c24a4084695d62d
NoahDB-dev <byernd31@uwgb.edu>
2025-04-28 10:56:05 -0500

    Adding code comments
    Adding code comments to the JavaScript files to make them more readable

--- classes.js
+++ classes.js
@@ -1,20 +1,28 @@
+//Creating the boundary class
 class Boundary {
+
+    //Setting the values for width and height. Since these will never change they are static
     static width = 32;
     static height = 32;
-
+    
+    //Creating the constructor for the boundary class
     constructor({position}) {
         this.position = position;
         this.width = 32;
         this.height = 32;
     }
 
+    //Setting the draw function. This is technically uneccesary, however, it makes it easier to trouble shoot.
     draw() {
-        ctx.fillStyle = 'rgba(255, 0, 0, 0)';
+        ctx.fillStyle = 'rgb(255, 0, 0)';
         ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
 }
 
+//Creating the Sprite class
 class Sprite {
+
+    //Creating the constructor for the Sprite class. It takes 7 objects as input, however, some have default values so they don't need to be specified
     constructor({position, velocity, image, frames = {xMax: 1, yMax: 1}, sizeAdjust = {width: 0, height: 0}, scale = {scaleX: 1, scaleY: 1}, animate = false}) {
         this.position = position;
         this.image = image;
@@ -24,7 +32,7 @@ class Sprite {
         this.animate = animate;
 
         this.frameElapsed = 0;
-        this.frameHold = 50;
+        this.frameHold = 10;
     
         this.image.onload = () => {
             this.width = (this.image.width - this.sizeAdjust.width/2) / this.frames.xMax * this.scale.scaleX;
@@ -32,10 +40,13 @@ class Sprite {
         }
     }  
 
+    //Creating the draw function for all the sprites
     draw() {
 
+        //Checking if the Sprite has an animation
         if (!this.animate) this.frames.val = 0;
         
+        //Actually drawing the image on the canvas
         ctx.drawImage(
             this.image,
             this.frames.val * (this.image.width / this.frames.xMax),
@@ -48,7 +59,10 @@ class Sprite {
             (this.image.height / this.frames.yMax) * this.scale.scaleY
         );
     
+        //Incrementing the frame counter
         this.frameElapsed++;
+
+        //If the frameElapsed has reached a threshold of frameHold and the sprite has an animation, then change the animation frame
         if (this.frameElapsed % this.frameHold === 0 && this.animate) {
             if (this.frames.val < this.frames.xMax - 1) {
                 this.frames.val++;
--- index.js
+++ index.js
@@ -1,20 +1,26 @@
+//Gathering the canvas and the context of the canvas from the html file
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
+//Setting the dimensions of the canvas
 canvas.width = 1050;
 canvas.height = 700;
 
+//Creating the array that will store the collisions data
 const collisionsMap = [];
 
+//Getting the values from the collissions array (Stored in collisions.js) and adding them to the collisions map array
 for(let i = 0; i < collisions.length; i += 60) {
     collisionsMap.push(collisions.slice(i, i + 60));
 }
 
+//Creating the offset needed to center the background in the correct position.
 const offset = {
     x: -203,
     y: -2120
 }
 
+//Creating the array that will store the boundary objects based off the information in the collisions array
 const boundaries = [];
 
 collisionsMap.forEach((row, i) => {
@@ -24,15 +30,19 @@ collisionsMap.forEach((row, i) => {
     })
 })
 
+//Creating the background image (AKA the town background)
 const image = new Image();
 image.src = './images/map/starterTown.png';
 
+//Creating the player image (AKA the character)
 const playerImage = new Image();
 playerImage.src = './images/character/characterSprites.png';
 
+//Creating the foreground image (AKA the things that should always be rendered on top)
 const foregroundImage = new Image();
 foregroundImage.src = './images/map/Foreground.png';
 
+//Creating the player object that will store information about the player (The class is Sprite)
 const player = new Sprite({
     position: {
         x: (canvas.width/2) - (347) / 2 + 110, 
@@ -40,8 +50,13 @@ const player = new Sprite({
     image: playerImage, 
     frames: {xMax: 4, yMax: 4},
     sizeAdjust: {width: 173, height: 101},
-    scale: {scaleX: 1.25, scaleY: 1.25}});
+    scale: {scaleX: 1.25, scaleY: 1.25}
+});
 
+//Changes the movement speed of the player (technically changes the speed that the background moves)
+const velocity = 2;
+
+//Creating the background object that will store information about the background (The class is Sprite)
 const background = new Sprite({ 
     position: {
         x: offset.x, 
@@ -49,6 +64,7 @@ const background = new Sprite({
     image: image
 })
 
+//Creating the foreground object that will store information about the foreground (The class is Sprite)
 const foreground = new Sprite({ 
     position: {
         x: offset.x, 
@@ -56,6 +72,7 @@ const foreground = new Sprite({
     image: foregroundImage
 })
 
+//Creating the keys object which will store which keys are being pressed as true or false values
 const keys = {
     w: { pressed: false },
     a: { pressed: false },
@@ -63,20 +80,25 @@ const keys = {
     d: { pressed: false }
 }
 
+//Creating an array that will store all the objects that move
 const movables = [background, ...boundaries, foreground];
 
+//Creating a function that checks if two rectangles with x and y values are colliding. (This really only works for the player since it is using arbitrary values for the calculation)
 function rectangularCollision({rectangle1, rectangle2}) {
     return (
         rectangle1.position.x + rectangle1.width >= rectangle2.position.x && 
-        rectangle1.position.x <= (rectangle2.position.x) -21 + rectangle2.width &&
-        rectangle1.position.y <= rectangle2.position.y -63 + rectangle2.height &&
-        rectangle1.position.y + rectangle1.height >= rectangle2.position.y
+        rectangle1.position.x + 30 <= (rectangle2.position.x) + rectangle2.width&&
+        rectangle1.position.y  + 60 <= rectangle2.position.y + rectangle2.height &&
+        rectangle1.position.y + rectangle1.height + 13 >= rectangle2.position.y
     )
 }
 
+//Creating the function that will actually draw the game on the canvas
 function animate() {
+    //By calling this we create an infinite loop while still only having to call the animate function once
     window.requestAnimationFrame(animate);
 
+    //Drawing the objects on the screen in the order that they should be drawn (background -> player -> foreground)
     background.draw();
     boundaries.forEach(boundary => {
         boundary.draw();
@@ -84,91 +106,121 @@ function animate() {
     player.draw();
     foreground.draw();
    
+    //Defining the "moving" variable and setting the default value to true
     let moving = true;
 
+    //Setting the actions that should happen if W is pressed and is the most recent key to be pressed
     if (keys.w.pressed && lastKey === 'w') {
+        //Telling the sprite's draw function that this sprite has an animation as well as where it is in the y direction
         player.frames.y = 3;
         player.animate = true;
-       // player.frameElapsed = 0;
        
+        //Checking the collisions of the player and boundaries
         for (let i = 0; i < boundaries.length; i++) {
             const boundary = boundaries[i];
-            if (rectangularCollision({rectangle1: player, rectangle2: {...boundary, position: {x: boundary.position.x, y: boundary.position.y + 0.5}}})) {
-                console.log('colliding')
+            if (rectangularCollision({rectangle1: player, rectangle2: {...boundary, position: {x: boundary.position.x, y: boundary.position.y + 2}}})) {
                 moving = false;
+                console.log("colliding");
                 break;
             }
         }
+
+        //If the player isn't colliding with anything then setting the position of all movables to be adjusted by the velocity
         if (moving) {
             movables.forEach(movable => {
-                movable.position.y += 0.5;
+                movable.position.y += velocity;
             })
         }
     }
+
+    //Setting the actions that should happen if A is pressed and is the most recent key to be pressed
     else if (keys.a.pressed && lastKey === 'a') {
+
+        //Telling the sprite's draw function that this sprite has an animation as well as where it is in the y direction
         player.frames.y = 1;
         player.animate = true;
-        //player.frameElapsed = 0;
         
+        //Checking the collisions of the player and boundaries
         for (let i = 0; i < boundaries.length; i++) {
             const boundary = boundaries[i];
-            if (rectangularCollision({rectangle1: player, rectangle2: {...boundary, position: {x: boundary.position.x + 0.5, y: boundary.position.y}}})) {
-                console.log('colliding')
+            if (rectangularCollision({rectangle1: player, rectangle2: {...boundary, position: {x: boundary.position.x + 2, y: boundary.position.y}}})) {
                 moving = false;
+                console.log("colliding");
                 break;
             }
         }
+
+        //If the player isn't colliding with anything then setting the position of all movables to be adjusted by the velocity
         if (moving) {
         movables.forEach(movable => {
-            movable.position.x += 0.5;
+            movable.position.x += velocity;
         })
         }
     }
+
+    //Setting the actions that should happen if S is pressed and is the most recent key to be pressed
     else if (keys.s.pressed && lastKey === 's') {
+
+        //Telling the sprite's draw function that this sprite has an animation as well as where it is in the y direction
         player.frames.y = 0;
-        //player.frameElapsed = 0;
         player.animate = true;
 
+        //Checking the collisions of the player and boundaries
         for (let i = 0; i < boundaries.length; i++) {
             const boundary = boundaries[i];
-            if (rectangularCollision({rectangle1: player, rectangle2: {...boundary, position: {x: boundary.position.x, y: boundary.position.y - 0.5}}})) {
-                console.log('colliding')
+            if (rectangularCollision({rectangle1: player, rectangle2: {...boundary, position: {x: boundary.position.x, y: boundary.position.y - 2}}})) {
                 moving = false;
+                console.log("colliding");
                 break;
             }
         }
+
+        //If the player isn't colliding with anything then setting the position of all movables to be adjusted by the velocity
         if (moving) {
             movables.forEach(movable => {
-                movable.position.y -= 0.5;
+                movable.position.y -= velocity;
             })
         }
     }
+
+    //Setting the actions that should happen if D is pressed and is the most recent key to be pressed
     else if (keys.d.pressed && lastKey === 'd') {
+        
+        //Telling the sprite's draw function that this sprite has an animation as well as where it is in the y direction
         player.frames.y = 2;
-        //player.frameElapsed = 0;
         player.animate = true; 
 
+        //Checking the collisions of the player and boundaries
         for (let i = 0; i < boundaries.length; i++) {
             const boundary = boundaries[i];
-            if (rectangularCollision({rectangle1: player, rectangle2: {...boundary, position: {x: boundary.position.x - 0.5, y: boundary.position.y}}})) {
-                console.log('colliding')
+            if (rectangularCollision({rectangle1: player, rectangle2: {...boundary, position: {x: boundary.position.x - 2, y: boundary.position.y}}})) {
                 moving = false;
+                console.log("colliding");
                 break;
             }
         }
+
+        //If the player isn't colliding with anything then setting the position of all movables to be adjusted by the velocity
         if (moving) {
             movables.forEach(movable => {
-                movable.position.x -= 0.5;
+                movable.position.x -= velocity;
             })
         }
     }
     
 }
+
+//Calling the animate function to start the animation loop
 animate();
 
-//the (e) stands for event
+//Creating the lastKey variable and setting the default value to be empty
 let lastKey = '';
+
+//Creating an event listener that listens for key presses. 
+//e stands for event
 window.addEventListener('keydown', (e) => {
+    
+    //Checking which key is pressed and executing the code for that key
     switch (e.key) {
         case 'w':
             keys.w.pressed = true;
@@ -192,7 +244,12 @@ window.addEventListener('keydown', (e) => {
             break;
     }
 });
+
+//Creating an event listener that listens for when a key is released
+//e stands for event
 window.addEventListener('keyup', (e) => {
+
+    //Checking which key was released and executing the code for that key
     switch (e.key) {
         case 'w':
             player.animate = false;
